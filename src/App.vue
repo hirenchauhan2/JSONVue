6e439a846feb810f8b655ef185ce0a45c449f943
Hiren Chauhan <hiren.maheshwari366@gmail.com>
2017-02-26 23:07:39 +0530

    :smile: First Version of extension...

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <tree-view :data="json" max-depth="3"></tree-view>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
 
 export default {
   name: 'app',
-  components: {
-    Hello
-  }
+  props: ['json']
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,44 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import TreeView from 'vue-json-tree-view'
+
+Vue.use(TreeView)
+
 import App from './App'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+function init (json) {
+  const d = document
+  // clean the DOM first
+  d.body.innerText = ''
+
+  const app = d.createElement('div')
+  app.setAttribute('id', 'app')
+  d.body.appendChild(app)
+
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    template: `<App :json="json" />`,
+    components: { App },
+    data () {
+      return {
+        json
+      }
+    }
+  })
+}
+
+(() => {
+  let json = document.body.innerText
+  if (json && json.length) {
+    try {
+      json = JSON.parse(json)
+      if (typeof json === 'object') {
+        init(json)
+      }
+    } catch (e) {
+      console.log('Not a JSON', json)
+    }
+  }
+})()
